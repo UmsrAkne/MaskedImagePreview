e69ba65b49b3fb2e942ffddaa1e16439c02c6583
UmsrAkne <sibata486@gmail.com>
2026-09-20 18:25:57 +0900

    init

---
+++ MaskedImagePreview/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MaskedImagePreview.Utils;
+
+namespace MaskedImagePreview.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "MaskedImagePreview";
+
+        public MainWindowViewModel()
+        {
+            AppLogger.Info("MainWindowViewModel created");
+        }
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
