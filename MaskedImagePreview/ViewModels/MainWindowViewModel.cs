881163e461b4f92e7978c97074e7dfe5a2d88c27
UmsrAkne <sibata486@gmail.com>
2026-09-20 18:49:26 +0900

    feat: Add drag-and-drop support to MainWindow
    - Integrated gong-wpf-dragdrop package.
    - Enabled drop handling in MainWindow with IDropTarget implementation.
    - Added file drag-and-drop functionality to preview area.

--- MaskedImagePreview/ViewModels/MainWindowViewModel.cs
+++ MaskedImagePreview/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
-﻿using MaskedImagePreview.Utils;
+﻿using System.Windows;
+using GongSolutions.Wpf.DragDrop;
+using MaskedImagePreview.Utils;
 
 namespace MaskedImagePreview.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
-    public class MainWindowViewModel : BindableBase
+    public class MainWindowViewModel : BindableBase, IDropTarget
     {
         private string title = "Masked Image Preview";
 
@@ -13,5 +15,27 @@ namespace MaskedImagePreview.ViewModels
         }
 
         public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+            if (dropInfo.Data is DataObject dataObject && dataObject.ContainsFileDropList())
+            {
+                dropInfo.Effects = DragDropEffects.Copy;
+                dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+            }
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            if (dropInfo.Data is DataObject dataObject && dataObject.ContainsFileDropList())
+            {
+                var files = dataObject.GetFileDropList();
+                var filePath = files[0];
+
+                AppLogger.Info($"Dropped file: {filePath}");
+
+                // 画像の読み込み処理...
+            }
+        }
     }
 }
