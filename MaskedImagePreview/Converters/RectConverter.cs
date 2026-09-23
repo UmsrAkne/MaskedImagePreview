a3e95ce8c7c406930477778c5063672c92e9fe90
UmsrAkne <sibata486@gmail.com>
2026-09-23 23:58:16 +0900

    feat: Add RectConverter and improve mask alignment
    - Introduced `RectConverter` to calculate mask image alignment dynamically.
    - Updated `MainWindow.xaml` to bind `ImageBrush.Viewport` for precise mask placement.
    - Replaced `ScrollViewer` with `Border` for better control over image transformations.
    - Enabled drag-to-pan functionality to adjust image offsets interactively.

---
+++ MaskedImagePreview/Converters/RectConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MaskedImagePreview.Converters
+{
+    public class RectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values is [double width and > 0, double height and > 0, ..])
+            {
+                return new Rect(0, 0, width, height);
+            }
+
+            return Rect.Empty;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
